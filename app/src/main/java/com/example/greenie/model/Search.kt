6d87396c68f642a9374d23f7c7c999cd43efac0c
GMN177 <gimmy177@icloud.com>
2025-04-04 20:03:30 +0200

    Add search name

--- app/src/main/java/com/example/greenie/model/Search.kt
+++ app/src/main/java/com/example/greenie/model/Search.kt
@@ -1,8 +1,7 @@
 package com.example.greenie.model
 
-import kotlinx.serialization.Serializable
-
 data class Search (
+    val name: String,
     val lng : Double,
     val lat : Double,
     val brightness : Float,
